1523e43fa9fed4f997922cc78601e34ed8a4985e
chenxinyang <43377267+dyxinyang@users.noreply.github.com>
2018-12-18 00:20:26 +0800

    Update wcount.py

--- pyassign4/wcount.py
+++ pyassign4/wcount.py
@@ -9,27 +9,15 @@ import re
 from urllib.request import urlopen
 from collections import Counter
 
-
-if len(sys.argv) == 3:
-    topn = int(sys.argv[2])###判断是否输入了所求的单词个数
-
-if len(sys.argv) > 1:
-    doc = urlopen(sys.argv[1])
-    docstr = doc.read()
-    jstr = docstr.decode()
-    lines = re.findall(r'\w+',str.lower(jstr))
-
-    def wcount(lines, topn=10):
-        """count words from lines of text string, then sort by their counts
-        in reverse order, output the topn (word count), each in one line.
-        """
-        result = Counter(lines)
-        for i in range(topn):
-    	    print(result.most_common(topn)[i][0]+\
-            " "*(12-len(result.most_common(topn)[i][0]))+\
-            str(result.most_common(topn)[i][1])+'\n')
-
-    print(wcount(lines,topn))
+def wcount(lines, topn=10):
+    """count words from lines of text string, then sort by their counts
+    in reverse order, output the topn (word count), each in one line.
+    """
+    result = Counter(lines)
+    for i in range(topn):
+	    print(result.most_common(topn)[i][0]+\
+        " "*(12-len(result.most_common(topn)[i][0]))+\
+        str(result.most_common(topn)[i][1])+'\n')
 
 
 if __name__ == '__main__':
@@ -39,3 +27,15 @@ if __name__ == '__main__':
         print('  url: URL of the txt file to analyze ')
         print('  topn: how many (words count) to output. If not given, will output top 10 words')
         sys.exit(1)
+    
+    if len(sys.argv) == 3:
+        topn = int(sys.argv[2])###判断是否输入了所求的单词个数
+
+    if len(sys.argv) > 1:
+        doc = urlopen(sys.argv[1])
+        docstr = doc.read()
+        jstr = docstr.decode()
+        lines = re.findall(r'\w+',str.lower(jstr)) 
+        
+    print(wcount(lines,topn))
+
